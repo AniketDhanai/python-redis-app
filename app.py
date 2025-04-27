ac96535e306aeeb583ff2c1d16d2b2a9349493c7
Aniket Raju Dhanai <96654182+AniketDhanai@users.noreply.github.com>
2025-04-27 13:00:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host=&apos;redis&apos;, port=6379)
+redis = Redis(host='redis', port=6379)
 
-@app.route(&apos;/&apos;)
+@app.route('/')
 def hello():
-    redis.incr(&apos;hits&apos;)
-    count = redis.get(&apos;hits&apos;).decode(&apos;utf-8&apos;)  # decode bytes to string
-    return f&quot;&quot;&quot;
-        &lt;html&gt;
-            &lt;head&gt;&lt;title&gt;Hello from Flask + Redis&lt;/title&gt;&lt;/head&gt;
-            &lt;body style=&quot;text-align: center; margin-top: 50px;&quot;&gt;
-                &lt;h1&gt;Hello Everyone!&lt;/h1&gt;
-                &lt;img src=&quot;/static/stickman.png&quot; alt=&quot;image not available&quot;&gt;
-                &lt;p&gt;You have been hit {count} times.&lt;/p&gt;
-            &lt;/body&gt;
-        &lt;/html&gt;
-    &quot;&quot;&quot;
-if __name__ == &quot;__main__&quot;:
-    app.run(host=&quot;0.0.0.0&quot;, debug=True)
+    redis.incr('hits')
+    count = redis.get('hits').decode('utf-8')  # decode bytes to string
+    return f"""
+        <html>
+            <head><title>Hello from Flask + Redis</title></head>
+            <body style="text-align: center; margin-top: 50px;">
+                <h1>Hello Everyone!</h1>
+                <img src="/static/image.png" alt="image not available">
+                <p>You have been hit {count} times.</p>
+            </body>
+        </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
